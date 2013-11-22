3597dfe235fcbe1bd4d8a42e826e009c5edb5809
Jiří Suchomel <jsuchome@suse.cz>
2013-11-22 13:49:46 +0100

    - do not change nscd cache value for OES (bnc#848051)

--- src/modules/Ldap.rb
+++ src/modules/Ldap.rb
@@ -2932,7 +2932,7 @@ module Yast
           "tls_checkpeer",
           @tls_checkpeer == "yes" ? nil : @tls_checkpeer
         )
-        WriteNscdCache(@start && @sssd)
+        WriteNscdCache(@start && @sssd) unless oes
       end
       if @start # ldap used for authentication
         # ---------- correct pam_password value for Novell eDirectory
